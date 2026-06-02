036da44df1bc7af683f0a6620d661be6a763a0a1
Vishal Mewada <vishalmewada.cs25@gmail.com>
2026-06-02 20:37:09 +0530

    Day 3:switch case program

---
+++ switch_cntrl_stat.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    
+switch (age)
+{
+case  14:
+    cout<<"you are fourteen"<<endl;
+    break;
+
+    case  19:
+    cout<<"you are nineteen"<<endl;
+    break;
+
+    case  2:
+    cout<<"you are 2"<<endl;
+    break;  
+default:
+    break;
+}
+}
