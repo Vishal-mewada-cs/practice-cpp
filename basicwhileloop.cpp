887c52edc7bec2ef9c42219bc0cfaa71061db941
Vishal Mewada <vishalmewada.cs25@gmail.com>
2026-06-06 16:03:23 +0530

    Day 7:program to print table of any number

---
+++ basicwhileloop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    cout<<"enter number for table=";
+    cin>>n;
+
+    while (i<=10)
+    {
+        cout<<n*i<<endl;
+        i++;
+
+        
+    }
+    return 0;
+}
