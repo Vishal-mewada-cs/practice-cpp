5184ff11c2220782402dbe2ce0d16108dacf01b9
Vishal Mewada <vishalmewada.cs25@gmail.com>
2026-06-17 19:51:38 +0530

    Day 17: A Perfect number Program

---
+++ perfect_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, sum = 0;
+    cout << "enter the value=";
+    cin >> n;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+        }
+    }
+    if (sum == n)
+    {
+        cout << "Perfect number";
+    }
+    else
+    {
+        cout << " Not perfect number";
+    }
+    return 0;
+}
